d996ed9ac44df9b3c4b77ba3ab521741a25bc345
Erick0105 <erickspot01@gmail.com>
2024-08-06 19:09:09 -0300

    função de pesquisa criada

--- exercicio-01-arrays/js/script.js
+++ exercicio-01-arrays/js/script.js
@@ -5,6 +5,7 @@ listaDados = [
 ];
 
 
+
 //* Função que mostra  as listas
 function mostrarDados() {
     
@@ -28,6 +29,8 @@ function mostrarDados() {
 
 document.getElementById("mostrar").addEventListener("click", mostrarDados);
 
+
+
 //* Função que adiciona dados na lista
 function addDados(nome,idade){
     listaDados.push({"nome": nome, "idade": idade});
@@ -39,18 +42,22 @@ document.getElementById("addDados").addEventListener("click", ()=>{
     novaIdade = document.getElementById("idIdade").value;
 
     if (novoNome === "" || novaIdade === ""){
-        document.getElementById("aviso").innerHTML = "Preencha os dois campos"
+        document.getElementById("aviso").textContent = "Preencha os dois campos"
     }else{
+        document.getElementById("aviso").textContent = ""
         addDados(novoNome,novaIdade);
-        document.getElementById("aviso").innerHTML = ""
     }
 });
+
+
 
 //* Parte que reverte as listas
 document.getElementById("reverse").addEventListener("click", ()=>{
     listaDados.reverse();
     mostrarDados();
 });
+
+
 
 //* Parte que organiza as listas por idade
 document.getElementById("ordenarIdade").addEventListener("click", ()=>{
@@ -59,9 +66,37 @@ document.getElementById("ordenarIdade").addEventListener("click", ()=>{
     mostrarDados();
 });
 
+
+
 //* Parte que organiza as listas por nome
 document.getElementById("ordenarNome").addEventListener("click", ()=>{
     listaDados.sort((a, b) => a.nome.localeCompare(b.nome)); //! Perguntar uma explicação melhor sobre
     console.log(listaDados);
     mostrarDados();
+});
+
+
+
+//* Função que pesquisa se tem um nome na lista
+function pesquisar(alvo){
+    
+    resultado = listaDados.find(procurado => procurado.nome === alvo);
+
+    
+    if (resultado !== undefined){
+        document.getElementById("aviso").textContent = "Usuário encontrado";
+    }else{
+        document.getElementById("aviso").textContent = "Usuário não encontrado";
+    }
+}
+
+document.getElementById("pesquisar").addEventListener("click", ()=>{
+    pessoasPesquisada = document.getElementById("idPesquisa").value
+    
+    if (pessoasPesquisada === ""){
+        document.getElementById("aviso").textContent = "Preencha o campo de pesquisa"
+    }else{
+        document.getElementById("aviso").textContent = ""
+        pesquisar(pessoasPesquisada)
+    }
 });
