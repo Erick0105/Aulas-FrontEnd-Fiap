6c5e195043223e442208802c65d2b0022f18287c
Erick0105 <erickspot01@gmail.com>
2024-08-09 21:54:59 -0300

    script do exercicio 3 concluido

--- exercicio-03-arrays/js/script.js
+++ exercicio-03-arrays/js/script.js
@@ -1,7 +1,17 @@
 
-arraySalarios = [5000,3500,1460,2000,1650 ,1250,2135,1900,2950,3785];
+arraySalarios = [4300,3500,1460,2000,1650 ,1250,2135,1900,2950,3785];
 
 //* Função para aumentar o sálario correspondente
 const salariosAtualizados = arraySalarios.map((salario) =>{
     return salario > 2000 ? salario + (salario *0.1):salario + (salario *0.15);
 });
+
+
+//* Filtrando os salarios maiores que 2500
+const filtrarSalarios = salariosAtualizados.filter((salariozinho)=> salariozinho >= 2500);
+
+
+//* Somando esses valores para um só usando Reduce
+const salarioSomados = filtrarSalarios.reduce((imposto,salarioAtual)=> imposto + salarioAtual);
+
+console.log(salarioSomados)
